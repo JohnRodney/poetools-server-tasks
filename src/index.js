6e2ad59f7506c791ed6fc908a867258770a94580
John Rodney <johnrodney@johns-mbp.attlocal.net>
2017-04-27 02:58:10 -0500

    bind to port to keep heroku happy

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+import http from 'http';
 import Runner from './tasks/poll-id';
 
 const runner = new Runner(null);
 
 runner.run();
+
+http.createServer((/* request, response */) => {}).listen(process.env.PORT || 5000);
