cc07569f93e9d0ea670c1c0b3f98737d33c9d44f
John Rodney <johnrodney@johns-mbp.attlocal.net>
2017-04-28 06:15:55 -0500

    hide those

--- settings.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  mongoConnectionString: 'mongodb://xenoraphorze:zerojiggy@cluster1-shard-00-00-ckpu4.mongodb.net:27017,cluster1-shard-00-01-ckpu4.mongodb.net:27017,cluster1-shard-00-02-ckpu4.mongodb.net:27017/Cluster1?ssl=true&replicaSet=Cluster1-shard-0&authSource=admin',
-};
